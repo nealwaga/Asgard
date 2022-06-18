28ad64043e1e8b5d9b7fb1fea0b6e5c2db2e843d
Neal Waga <waganeal@gmail.com>
2022-06-18 18:02:01 +0300

    created second model

--- mtaa/models.py
+++ mtaa/models.py
@@ -12,3 +12,25 @@ class Location(models.Model):
         return self.name
 
   
+class Hood(models.Model):
+    # hood_photo = models.ImageField(upload_to='hoods/')
+    hood_photo = CloudinaryField('hood_photo')
+    hood_name = models.CharField(max_length=100, null=True)
+    occupants_count = models.PositiveIntegerField(default=0)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
+    @classmethod
+    def get_hoods(cls):
+        hoods = Hood.objects.all()
+        return hoods
+
+    @classmethod
+    def search_hood(cls,hood_search):
+        hoods = cls.objects.filter(id__icontains = hood_search)
+        return hoods
+
+    class Meta:
+        ordering = ['hood_name']
+
+
