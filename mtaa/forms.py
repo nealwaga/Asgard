a14df5f7451092f8a59a0bb828b83d0fb82a20c4
Neal Waga <waganeal@gmail.com>
2022-06-18 18:13:08 +0300

    created sign-up form

--- mtaa/forms.py
+++ mtaa/forms.py
@@ -20,3 +20,10 @@ class BusinessForm(forms.ModelForm):
     class Meta:
         model = Business
         exclude = ['user','hood']
+        
+        
+class SignupForm(UserCreationForm):
+    email = forms.EmailField(max_length=200, help_text='Required')
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
