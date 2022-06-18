96e12421652e27a6be861191b6487010466e4f66
Neal Waga <waganeal@gmail.com>
2022-06-18 18:40:34 +0300

    updated urls

--- hoods/urls.py
+++ hoods/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls import include
 from django.contrib.auth import views
 
