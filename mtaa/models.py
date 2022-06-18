73083d88d1484c1f796d4cd4f8b917832e166cab
Neal Waga <waganeal@gmail.com>
2022-06-18 18:03:48 +0300

    created fourth model

--- mtaa/models.py
+++ mtaa/models.py
@@ -49,3 +49,32 @@ class Business(models.Model):
         return business
 
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,null=True)
+    # profile_photo= models.ImageField(upload_to='profiles/',null=True)
+    profile_photo = CloudinaryField('profiles')
+    bio = models.CharField(max_length=240, null=True)
+    email = models.CharField(max_length=100, blank=True, null=True)
+
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+        # post_save.connect(create_user_profile, sender=User)
+
+    @receiver(post_save, sender=User)
+    def update_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+        instance.profile.save()
+
+    @classmethod
+    def get_profile(cls):
+        profile = Profile.objects.all()
+        return profile
+
+    class Meta:
+        ordering = ['user']
+
+
+
