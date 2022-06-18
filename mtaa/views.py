c19adf92c55fe3829f064498b793cab8427f7b74
Neal Waga <waganeal@gmail.com>
2022-06-18 18:15:11 +0300

    imported modules

--- mtaa/views.py
+++ mtaa/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render, redirect, get_object_or_404
-
+from django.contrib.auth.decorators import login_required
+from .models import *
+from .forms import *
+from django.contrib.auth import login, authenticate
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from django.contrib.auth.models import User
+from django.core.mail import EmailMessage
+from django.contrib import messages
 
 # Create your views here.
