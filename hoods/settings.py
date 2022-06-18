3c624b77b52ef24f531eb53d39872ba15bbd3ac6
Neal Waga <waganeal@gmail.com>
2022-06-18 20:21:42 +0300

    connected to postgres

--- hoods/settings.py
+++ hoods/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'tinymce',
+    'django_registration',
 ]
 
 #Cloudinary Classes
@@ -96,10 +97,19 @@ WSGI_APPLICATION = 'hoods.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'hoods',
+        'USER': 'neal',
+        'PASSWORD': 'Wneal9.'
     }
 }
 
