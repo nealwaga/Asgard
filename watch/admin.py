688577c1913a978ec8fd3139733e5ddd99901b40
Neal Waga <waganeal@gmail.com>
2022-06-20 11:50:40 +0300

    added models to admin page

--- watch/admin.py
+++ watch/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(NeighbourHood)
+admin.site.register(Profile)
+admin.site.register(Business)
+admin.site.register(Post)
+admin.site.register(Authority)
+admin.site.register(Health)
