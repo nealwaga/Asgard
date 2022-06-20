a2ff898f250d2c106bbe9a1773628f5d93c53be4
Neal Waga <waganeal@gmail.com>
2022-06-20 11:42:10 +0300

    connected to postgres

---
+++ neighbourhood/urls.py
@@ -0,0 +1,30 @@
+"""neighbourhood URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import include
+from django.contrib.auth import views 
+from django.contrib.auth.views import LoginView, logout_then_login, LogoutView
+
+
+# Create urls here
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('watch.urls')),
+    path('accounts/', include('registration.backends.simple.urls')),
+    path('logout/', views.LogoutView.as_view(next_page='login'), name='logout'),
+]
