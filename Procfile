8ca8ce6df553460e33e05062f21df8f7c0c3c089
Neal Waga <waganeal@gmail.com>
2022-06-20 19:14:35 +0300

    deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn neighbourhood.wsgi --log-file -
--- neighbourhood/settings.py
+++ neighbourhood/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1v_061uqddc6$m3+c@4xp%jw)vjig7xdzqrl=!v+#&ce0ib^3k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['asgard-watch.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
