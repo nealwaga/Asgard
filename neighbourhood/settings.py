d0a111be1dfc714be33b909be1ba8578e76513a5
Neal Waga <waganeal@gmail.com>
2022-06-20 19:51:49 +0300

    deployment

--- neighbourhood/settings.py
+++ neighbourhood/settings.py
@@ -98,20 +98,20 @@ WSGI_APPLICATION = 'neighbourhood.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
-       'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'watch',
-        'USER': 'neal',
-    'PASSWORD':'Wneal9.',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'watch',
+#         'USER': 'neal',
+#     'PASSWORD':'Wneal9.',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
